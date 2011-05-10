cf3fac25449af47c0e33de101e4f31e475849379
aler@russia.ru <aler@Alexandrs-iMac.local>
2011-05-10 18:08:53 +0400

    Update

--- Scripts/Core/Main.js
+++ Scripts/Core/Main.js
@@ -27,45 +27,6 @@ atom.dom(function () {
         }
     });
     
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // agency.worker(15).call(12);
-    // 
-    // 
-    // 
-    // agency.worker(6).call(3);
-    // agency.worker(6).call(3);
-    // agency.worker(6).call(3);
-    // agency.worker(6).call(3);
-    // agency.worker(6).call(3);
-    // agency.worker(6).call(3);
-    // agency.worker(6).call(3);
-    // agency.worker(6).call(3);
-    // agency.worker(6).call(3);
-    // agency.worker(6).call(3);
-    // 
-    // 
-    // 
-    // agency.worker(0).call(1);
-    // agency.worker(0).call(1);
-    // 
-    // agency.worker(14).call(17);
-    // agency.worker(14).call(17);
-    // agency.worker(14).call(17);
-    
     var libcanvas = new LibCanvas('canvas', { fps: 30 })
         .listenMouse()
         .size({
@@ -80,13 +41,13 @@ atom.dom(function () {
         .addFunc( function () { this.update(); })
         .addFunc( function () {
             if (!mousePressed) return;
-            this.ctx.restore();
             this.ctx.save();
             mouseX      = this.mouse.point.clone().x - wwidth  / 2;
             mouseY      = this.mouse.point.clone().y - wheight / 2;
             console.log(this.mouse.point.clone().x , this.mouse.point.clone().y);
             angle       = ( Math.atan2(mouseY, mouseX) / Math.PI * 180 + 90 );//.degree();
             this.ctx.rotate(angle.degree(), agency.graph().transform().point);
+            this.ctx.restore();
         });
         
     libcanvas.mouse.addEvent({
