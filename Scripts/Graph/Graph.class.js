2cdac3c19efc4b7257ed2b1dfab772e1d57c8f39
aler@russia.ru <aler@Alexandrs-iMac.local>
2011-05-10 17:55:52 +0400

    Update

--- Scripts/Graph/Graph.class.js
+++ Scripts/Graph/Graph.class.js
@@ -112,7 +112,7 @@ var Graph = atom.Class({
                     var random = this.random;
                     random = i % 2 == 0 ? -random : random;
                     console.log();
-                    random = key % 2 == 0 ? random * Math.atan(parseInt(key)) * 3 : random * Math.atan(parseInt(key)) * 2
+                    random = key % 2 == 0 ? random * Math.atan(parseInt(key)) * 2 : random * Math.atan(parseInt(key)) * 1
                     ctx.moveTo(s.x, s.y);
                     ctx.bezierCurveTo(s.x, s.y, m.x + random + i, m.y + random + i, f.x, f.y);
                 }
